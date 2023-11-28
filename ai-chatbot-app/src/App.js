a6ddbdc36c185cb47731672e4bd596a8fe080ee0
riki1214 <46196500+riki1214@users.noreply.github.com>
2023-11-28 23:10:42 +0100

    no message

---
+++ ai-chatbot-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Chatbot from './Chatbot';
+
+function App() {
+  return (
+    <div>
+      <h1>TecnoBot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
